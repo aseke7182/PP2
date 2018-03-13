c3983e9a2a70e913601ce5529a7d7a706c1d088b
aseke7182 <askar_pvl_kz@mail.ru>
2018-03-14 00:49:42 +0600

    yeah

---
+++ Week 10/Game/Game/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Game
+{
+    partial class MyGameForBonusPoint
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 400;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 423);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score: 0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "GAME OVER";
+            this.label2.Visible = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(1, -3);
+            this.trackBar1.Maximum = 30;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(1, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(120, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Your ";
+            this.label3.Visible = false;
+            // 
+            // MyGameForBonusPoint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(334, 448);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MyGameForBonusPoint";
+            this.Text = "My Game For Bonus Point";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Week 10/Game/Game/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class MyGameForBonusPoint : Form
+    {
+        public MyGameForBonusPoint()
+        {
+            InitializeComponent();
+        }
+        Button b = new Button();
+        int cnt = 0;
+        List<Button> list = new List<Button>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            b.Location = new Point(0, Height -100);
+            b.Size = new Size(30, 30);
+            Controls.Add(b);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            cnt++;
+            Random rrr = new Random();
+            int a = rrr.Next(0, Width-60);
+            Button btn = new Button();
+            btn.Location = new Point(a, 0);
+            btn.Size = new Size(30, 30);
+            btn.BackColor = System.Drawing.SystemColors.Highlight;
+            Controls.Add(btn);
+            list.Add(btn);
+            label1.Text = "Score: "+ cnt.ToString();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if(Math.Abs(list[i].Location.X - b.Location.X)<= b.Width && Math.Abs(list[i].Location.Y - b.Location.Y) <= b.Height)
+                {
+                    trackBar1.Hide();
+                    b.Hide();
+                    for(int j=0; j<list.Count; j++)
+                    {
+                        list[j].Hide();
+                    }
+                    timer1.Stop();
+                    timer2.Stop();
+                    label2.Visible = true;
+                    label3.Text += label1.Text.ToString();
+                    label1.Visible = false;
+                    label3.Visible = true;
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            for(int i=0; i < list.Count; i++)
+            {
+                if(list[i].Location.Y + 5 >= Height-100)
+                {
+                    list[i].Visible = false;
+                }
+                list[i].Location = new Point(list[i].Location.X,list[i].Location.Y +5);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            b.Location = new Point(trackBar1.Value*10, b.Location.Y);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
